5dbe0592bc74d9c4e7cb0f93cefae0adf09cb239
MadhuriChandraSekhar <112499150+MadhuriChandraSekhar@users.noreply.github.com>
2022-10-04 08:45:49 -0500

    Update Program.java

--- src/test/java/stepDefinition/Program.java
+++ src/test/java/stepDefinition/Program.java
@@ -1,7 +1,10 @@
 package stepDefinition;
 import static org.testng.Assert.assertEquals;
 import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -27,36 +30,45 @@ public class Program {
 	public void user_should_see_a_heading_with_text_on_the_page(String managePgmHeading) {
 		programpage.headingTxt(managePgmHeading);
 		assertEquals(managePgmHeading, "Manage Program");
-		System.out.println("pass");
+		System.out.println("Manage Program is the headig text-pass");
 	}
 
 	@Given("User is logged on to LMS website")
 	public void user_is_logged_on_to_lms_website() {
-		//programpage =new ManageProgramPage(driver);
-		//programpage.validlogin("testId","Sheet1");  
+	
 	}
 
 	@Then("User should see text as {string} below the table")
 	public void user_should_see_text_as_below_the_table(String txtBelowTable) {
-		programpage.textBelowTable(txtBelowTable);
-		Assert.assertEquals(txtBelowTable,"Showing 1 to 5 of 27 entries");
-		System.out.println("pass"); 
+		programpage.textBelowTable(txtBelowTable);	
+		String txtfromPage=driver.findElement(By.xpath("//p-table//span[@class='p-paginator-current ng-star-inserted']")).getText();
+		System.out.println(txtfromPage);	
+		boolean Matched=txtBelowTable.contains("Showing 1 to 5 of ");
+		System.out.println(Matched); 
+
 	}
+
 	@Then("User should see the footer as {string}")
 	public void user_should_see_the_footer_as(String totalNoOfPrograms) {
 		programpage.footer(totalNoOfPrograms);
-		Assert.assertEquals(totalNoOfPrograms, "In total there are 27 Programs.");     
+		String totalPrograms=driver.findElement(By.xpath("//p-table//div[@class='p-d-flex p-ai-center p-jc-between ng-star-inserted']")).getText();
+		System.out.println(totalPrograms);
+		boolean Matched=totalNoOfPrograms.contains("In total there are ");
+		System.out.println(Matched);
 	}
+
 	@Then("User should see the Delete button on the top left hand side as Disabled")
 	public void user_should_see_the_delete_button_on_the_top_left_hand_side_as_disabled() {
 		programpage.disabledDelButton();   
 	}
 
-	@Then("Verify that number of records \\(rows of data in the table) {string}")
+	@Then("Verify that number of records {string}")
 	public void verify_that_number_of_records_rows_of_data_in_the_table(String noofrowsintable) {
 		programpage.NoOfRowsInTable();
-		//Assert.assertEquals(entry, "5 rows of data in table");
-		System.out.println("Pass, 5 rows of data displayed");
+		String txtBelowTable=driver.findElement(By.xpath("//p-table//span[@class='p-paginator-current ng-star-inserted']")).getText();
+		boolean MatchedRows=txtBelowTable.contains("Showing 1 to 5");
+		System.out.println(MatchedRows);
+
 	}
 	@Then("Text box used for search has text as {string}")
 	public void text_box_used_for_search_has_text_as(String textInSearchfield) {
@@ -65,7 +77,7 @@ public class Program {
 		Assert.assertEquals(textInSearchfield, Srch);
 		System.out.println("Pass");
 	}
-	@Given("User is on Program page")
+	@When("User is on Program page")
 	public void user_is_on_program_page() {
 		System.out.println("User is in program page");
 		programpage =new ManageProgramPage(driver);
@@ -99,13 +111,12 @@ public class Program {
 	public void user_enters_status_phrase_into_search_box(String status) {
 		status="Active";
 		programpage.searchByProgramStatus(status);
-
 	}
-
 	@Then("Entries for Status string are shown.")
 	public void entries_for_status_phrase_are_shown() {
 		System.out.println("Entries for entered status are shown");
 	}
+
 	@When("User selects Program using checkbox")
 	public void user_selects_program_using_checkbox() {
 		//programpage =new ManageProgramPage(driver);
@@ -226,44 +237,57 @@ public class Program {
 		programpage.deleteFeature();
 		programpage.deleteYes();
 	}
-	@When("User clicks <A New Program> button")
-	public void user_clicks_a_new_program_button() {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		programpage.addNewPgm();
+	////add new program 
+	@Given("User is on Program Details form to add new program after valid login with {string} and {string}")
+	public void user_is_on_program_details_form_to_add_new_program(String testId, String sheetName) {
+		programpage=new ManageProgramPage(driver);		
+		programpage.validlogin("testId", "sheet1");
+	}
+
+	@When("User clicks {string} button")
+	public void user_clicks_button(String string) {
+		programpage.clickAddNewProgramBtn();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	    
 	}
 
 	@Then("User lands on Program Details form to add new program.")
 	public void user_lands_on_program_details_form_to_add_new_program() {
-		System.out.println("User lands on Program Details form to add new program");
-	}
-	@When("User clicks <Save> button without entering data")
-	public void user_clicks_save_button_without_entering_data() {
-		programpage.saveNewPgmDetails();
+	    System.out.println("User should be navigated to Program detials window");
+	    
 	}
 
+	@Given("User is on Program Details form for adding new program")
+	public void user_is_on_program_details_form_for_adding_new_program() {
+		programpage =new ManageProgramPage(driver);
+		programpage.validlogin("testId", "sheet1");
+		programpage.clickAddNewProgramBtn();	    
+	}
+	@When("User clicks {string} button without entering data")
+	public void user_clicks_button_without_entering_data(String string) {
+	    programpage.clickSavewithEmptyFiledValues();
+
+	}
 	@Then("User gets message {string}")
-	public void user_gets_message(String string) {
-		System.out.println("Name is required");
+	public void user_gets_message(String alertMsg) {
+	    programpage.nameIsRequiredMsg(alertMsg);
+	    Assert.assertEquals(alertMsg, "Name is required.");
+	    System.out.println("pass");  
+	}	
+	@When("User enters {string} {string} and {string} and clicks {string} button")
+	public void user_enters_Name_Description_and_Status_and_clicks_save_button(String s1,String s2, String s3, String s4) {
+	    programpage.addAllProgramDetailsSave(s1,s2,s3,s4);
+	    
 	}
-	@When("User enters <Name> in text box")
-	public void user_enters_name_in_text_box() {
-	}
-
-	@Then("User can see {string} entered")
-	public void user_can_see_entered(String string) {
-	}
-
-	@When("User enters <Description> in text box")
-	public void user_enters_description_in_text_box() {
-	}
-
-	@When("User selects <Status> using radiobutton")
-	public void user_selects_status_using_radiobutton() {
-	}
-	@Then("User can see {string} status selected")
-	public void user_can_see_status_selected(String string) {
-	}
-
+	@Then("User can see {string} message on screen")
+	public void user_can_see_message_on_screen(String string) {
+	    //new program info should be added under programs table as the latest entry	
+	    String namein = driver.findElement(By.xpath("//tbody/tr[1]/td[2]")).getText();
+	    System.out.println("Success message program updated should appear");
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));	
+	    System.out.println(namein);
+	    
+}
+	//Multi Deletion
 	@When("User selects more than one Program using checkbox")
 	public void user_selects_more_than_one_program_using_checkbox() {
 	
@@ -319,38 +343,53 @@ public class Program {
 		System.out.println("User can see  All Programs Deleted message");
 
 	}
-}
-/*
-	//Pagination
+
+ //Pagination
 	@When("User is on first page of Manage Program")
 	public void user_is_on_first_page_of_manage_program() {
+		programpage=new ManageProgramPage(driver);		
+		programpage.validlogin("testId", "sheet1");
 		programpage.clickPagiLeft();
 	}
 	@Then("Verify that previous link is disabled")
-	public void verify_that_previous_link_is_disabled() {
+	public void verify_that_previous_link_is_disabled() {	
 		System.out.println("Pagination left arrow is disabled");
 	}
 	@Given("User is on the page number {string}")
 	public void user_is_on_the_page_number(String string) {
+		programpage=new ManageProgramPage(driver);
+		programpage.validlogin("testId", "sheet1");
+		System.out.println("user in page 1 of programs page");
 	}
 
 	@When("User clicks navigateRight > button")
 	public void user_clicks_navigate_right_button() {
+		programpage.clickPagiRight();
 	}
 
 	@Then("User navigated to page number {string}")
 	public void user_navigated_to_page_number(String string) {
+		
+		System.out.println("user navigated to page 2 of programs page");
+		
 	}
 	@When("User clicks navigateLeft < button")
 	public void user_clicks_navigate_left_button() {
+		programpage.clickPagiLeft();
 	}
 
 	@When("User is on last page of Manage Program")
 	public void user_is_on_last_page_of_manage_program() {
+		WebElement navi=driver.findElement(By.xpath("//button[@class='p-paginator-next p-paginator-element p-link p-ripple p-disabled']"));
+		boolean click=navi.isDisplayed();
+		System.out.println("navigation button is present");
 	}
 	@Then("Verify that next link is disabled")
 	public void verify_that_next_link_is_disabled() {
+		System.out.println("navigation disbaled");
 }
+	//Asc Dsc
+	
 	@Given("User is on the Program page")
 	public void user_is_on_the_program_page() {
 
@@ -398,5 +437,5 @@ public class Program {
 	}
 	@Then("User can see the results in descending order of Program status")
 	public void user_can_see_the_results_in_descending_order_of_program_status() {
-	}*/
-
+	}
+}
